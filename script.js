00e3602a6b369074102ff75ec75ed25728d77fe4
Zubaira Bashir <shabishabi5033@gmail.com>
2026-04-25 01:19:06 -0700

    Add JS function 2

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function showGreeting() {
     document.getElementById('output').innerText = "Hello! This is Function 1.";
 }
 
+function showDate() {
+    document.getElementById('output').innerText = "Today's date is: " + new Date().toLocaleDateString();
+}
+
 document.getElementById('btn1').addEventListener('click', showGreeting);
+document.getElementById('btn2').addEventListener('click', showDate);
